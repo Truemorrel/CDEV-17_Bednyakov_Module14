5173c99f00b4bd0e67fd7adc7889f44c0a06a76c
Sergey Bednyakov <sbednyakov@gmail.com>
2022-07-13 02:50:39 +0300

    Add project files.

---
+++ Task_14_1_4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_14_1_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // выбрать имена на букву А и отсортировать в алфавитном порядке.
+            string[] people = { "Анна", "Мария", "Сергей", "Алексей", "Дмитрий", "Ян" };
+            SortedSet<string> aName = new SortedSet<string>(people);
+            aName.RemoveWhere( n => !n.ToUpper().StartsWith('А'));
+            foreach (var item in aName)
+            { 
+                Console.WriteLine(item);
+            };
+         }
+    }
+}
